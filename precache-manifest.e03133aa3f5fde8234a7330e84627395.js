84d38077ac435124f4c5bab3517b616e14afadcb
Jonathan Lam <jonathanlam@users.noreply.github.com>
2022-12-05 15:48:10 +1100

    Updates

---
+++ precache-manifest.e03133aa3f5fde8234a7330e84627395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af86193f4d2af7081fae6653a5b27233",
+    "url": "/vlive-frontend/index.html"
+  },
+  {
+    "revision": "391375b2b44f577f6d9d",
+    "url": "/vlive-frontend/static/css/main.7e96b1e2.chunk.css"
+  },
+  {
+    "revision": "f67b4f2a505fba8ac76d",
+    "url": "/vlive-frontend/static/js/2.60191665.chunk.js"
+  },
+  {
+    "revision": "351a144780b80d8155819a4e07746964",
+    "url": "/vlive-frontend/static/js/2.60191665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391375b2b44f577f6d9d",
+    "url": "/vlive-frontend/static/js/main.3573bd86.chunk.js"
+  },
+  {
+    "revision": "d49be00445e83820a787",
+    "url": "/vlive-frontend/static/js/runtime-main.ee14587e.js"
+  }
+]);
