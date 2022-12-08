2e15e3d67c1011dc25e072b00a16059241617597
Jonathan Lam <jonathanlam@users.noreply.github.com>
2022-12-08 14:44:16 +1100

    add vods to static folder

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -26,7 +26,7 @@ const Board = () => {
 
   useEffect(() => {
     axios
-      .get(`https://vlivearchive.com/data/vods/${artist_name}_vods.json`)
+      .get(`/static/vods/${artist_name}_vods.json`)
       .then(function (response) {
         console.log(response.data);
         setVodList(response.data);
