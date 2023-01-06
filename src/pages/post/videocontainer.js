53eedd086c201577ce7bd871a10adae0af03c496
Jonathan Lam <jonathanlam@users.noreply.github.com>
2023-01-06 21:36:02 +1100

    unused import

--- src/pages/post/videocontainer.js
+++ src/pages/post/videocontainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 import CommentIcon from "../../components/icons/comment";
 import HeartIcon from "../../components/icons/heart";
 import * as dayjs from "dayjs";
