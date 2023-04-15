1de7b41d2bb99d5182075fadfcde5278a2a51148
Jonathan Lam <jonathanlam@users.noreply.github.com>
2023-04-15 22:21:02 +1000

    revert player

--- src/pages/post/VideoPlayer.js
+++ src/pages/post/VideoPlayer.js
@@ -1,14 +1,13 @@
 import React from "react";
-// import ReactPlayer from "react-player";
-import { Player } from "react-tuby";
-import "react-tuby/css/main.css";
+import ReactPlayer from "react-player";
+//import { Player } from "react-tuby";
+//import "react-tuby/css/main.css";
 
 function formatSubLabel(caption) {
   var label = caption.label;
   if (caption.type === "cp") label += " (official)";
   if (caption.type === "auto") label += " (auto)";
   if (caption.type === "fan") label += " (fan)";
-
   if (caption.fanName !== "") label += ` by ${caption.fanName}`;
   return label;
 }
@@ -39,34 +38,34 @@ const VideoPlayer = ({ post, channel, thumbnail_url }) => {
   var subtitles = post?.captions || [];
 
   return (
-    // <ReactPlayer
-    //   url={video_url}
-    //   height="400px"
-    //   width="100%"
-    //   controls={true}
-    //   config={{
-    //     file: {
-    //       attributes: {
-    //         crossOrigin: true,
-    //       },
-    //       tracks: subtitles.map((e) => ({
-    //         kind: "subtitles",
-    //         src: `/subtitles/${e.file_name}`,
-    //         srcLang: e.language,
-    //         label: formatSubLabel(e),
-    //       })),
-    //     },
-    //   }}
-    // />
-    <Player
-      src={video_url}
-      //poster={thumbnail_url}
-      //subtitles={subtitles.map((e) => ({
-      //  url: `/subtitles/${e.file_name}`,
-      //  lang: e.language,
-      //  language: formatSubLabel(e),
-      //}))}
+    <ReactPlayer
+      url={video_url}
+      height="400px"
+      width="100%"
+      controls={true}
+      config={{
+        file: {
+          attributes: {
+            crossOrigin: true,
+          },
+          tracks: subtitles.map((e) => ({
+            kind: "subtitles",
+            src: `/subtitles/${e.file_name}`,
+            srcLang: e.language,
+            label: formatSubLabel(e),
+          })),
+        },
+      }}
     />
+    //<Player
+    //  src={video_url}
+    //  poster={thumbnail_url}
+    //  subtitles={subtitles.map((e) => ({
+    //    url: `/subtitles/${e.file_name}`,
+    //    lang: e.language,
+    //    language: formatSubLabel(e),
+    //  }))}
+    ///>
   );
 };
 
