ac98d7745f315eb41864e867f9c525edb237d62b
Jonathan Lam <jonathanlam@users.noreply.github.com>
2023-04-16 22:24:02 +1000

    player

--- src/pages/post/VideoPlayer.js
+++ src/pages/post/VideoPlayer.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactPlayer from "react-player";
-//import { Player } from "react-tuby";
-//import "react-tuby/css/main.css";
+//import ReactPlayer from "react-player";
+import { Player } from "react-tuby";
+import "react-tuby/css/main.css";
 
 function formatSubLabel(caption) {
   var label = caption.label;
@@ -38,34 +38,34 @@ const VideoPlayer = ({ post, channel, thumbnail_url }) => {
   var subtitles = post?.captions || [];
 
   return (
-    <ReactPlayer
-      url={video_url}
-      height="400px"
-      width="100%"
-      controls={true}
-      config={{
-        file: {
-          attributes: {
-            crossOrigin: true,
-          },
-          tracks: subtitles.map((e) => ({
-            kind: "subtitles",
-            src: `/subtitles/${e.file_name}`,
-            srcLang: e.language,
-            label: formatSubLabel(e),
-          })),
-        },
-      }}
-    />
-    //<Player
-    //  src={video_url}
-    //  poster={thumbnail_url}
-    //  subtitles={subtitles.map((e) => ({
-    //    url: `/subtitles/${e.file_name}`,
-    //    lang: e.language,
-    //    language: formatSubLabel(e),
-    //  }))}
+    //<ReactPlayer
+    //  url={video_url}
+    //  height="400px"
+    //  width="100%"
+    //  controls={true}
+    //  config={{
+    //    file: {
+    //      attributes: {
+    //        crossOrigin: true,
+    //      },
+    //      tracks: subtitles.map((e) => ({
+    //        kind: "subtitles",
+    //        src: `/subtitles/${e.file_name}`,
+    //        srcLang: e.language,
+    //        label: formatSubLabel(e),
+    //      })),
+    //    },
+    //  }}
     ///>
+    <Player
+      src={video_url}
+      poster={thumbnail_url}
+      subtitles={subtitles.map((e) => ({
+        url: `/subtitles/${e.file_name}`,
+        lang: e.language,
+        language: formatSubLabel(e),
+      }))}
+    />
   );
 };
 
